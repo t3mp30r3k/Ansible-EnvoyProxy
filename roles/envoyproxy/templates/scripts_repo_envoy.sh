983c43222e25d85b6ffdf8313e3f24e549cc0904
t3mp30r3k <rusdianto@gmail.com>
2022-08-11 20:37:56 +0700

    Update scripts_repo_envoy.sh

--- roles/envoyproxy/templates/scripts_repo_envoy.sh
+++ roles/envoyproxy/templates/scripts_repo_envoy.sh
@@ -1,8 +1,7 @@
-#/bin/sh
 
+#!/bin/sh
 curl -sL 'https://deb.dl.getenvoy.io/public/gpg.8115BA8E629CC074.key' | sudo gpg --dearmor -o /usr/share/keyrings/getenvoy-keyring.gpg
 
 #Verify the keyring - this should yield "OK"
-
 echo a077cb587a1b622e03aa4bf2f3689de14658a9497a9af2c427bba5f4cc3c4723 /usr/share/keyrings/getenvoy-keyring.gpg | sha256sum --check
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/getenvoy-keyring.gpg] https://deb.dl.getenvoy.io/public/deb/debian $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/getenvoy.list
